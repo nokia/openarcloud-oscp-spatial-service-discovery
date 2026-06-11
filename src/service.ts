3213fa90b382b599030d96028324686239a95ffa
soeroesg <gabor.soros@gmail.com>
2026-06-11 19:42:19 +0200

    fix compatibility problem with noise-protocol/dh

--- src/service.ts
+++ src/service.ts
@@ -3,6 +3,7 @@ import request from "request-promise";
 import { Element } from "./models/osm_json.interface";
 import { SsrDto } from "./models/ssr.dto";
 import { validateOrReject } from "class-validator";
+import "./noise-protocol-compat";
 import kappa from "kappa-core";
 import ram from "random-access-memory";
 import memdb from "memdb";
